b7163d838cdc144631b8c058c2cd4d1cda27263d
aZenGal <mohamedalitrabelsi388@yahoo.fr>
2024-05-02 23:10:31 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,13 @@
 
 DEPENDENCIES="nmap nikto git python3-pip"
 
-# Verify the OS witch is using and install on the right one
-if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then ##If need more distributions just add more
+# Vérifier que le système d'exploitation est Kali Linux
+if [ "$(grep 'Kali' /etc/issue)" ]; then
+    sudo apt-get update
     sudo apt-get install $DEPENDENCIES -y
-elif [ "$(grep -Ei 'redhat|centos' /etc/*release)" ]; then ##To install on RPM CentOS/RedHat
-    os_version=$(cut -d ':' -f5 < /etc/system-release-cpe)
-    if [ "$os_version" == 8 ]; then
-        sudo dnf install https://extras.getpagespeed.com/release-el8-latest.rpm -y
-        sudo dnf install $DEPENDENCIES -y
-    elif [ "$os_version" == 7 ]; then
-        sudo rpm -Uvh http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-20.el7.art.noarch.rpm
-        sudo rpm  -Uvh http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.art.noarch.rpm
-        sudo rpm -Uvh  http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.art.noarch.rpm
-        sudo yum install $DEPENDENCIES -y
-    fi  
+else
+    echo "Ce script est uniquement destiné à être exécuté sur Kali Linux"
+    exit 1
 fi
 
 pip3 install --user -r conf/requirements.txt
